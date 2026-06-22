7025ebe3db5de3ad0573feb3e6ccd4f47488398b
Anjali Jaiswal <anjalijaiswal19994@gmail.com>
2026-06-22 15:55:35 +0530

    Fix blacklist model import

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
 const emailService = require("../services/email.service")
-const tokenBlackListModel = require("../models/blackList.model")
+const tokenBlackListModel = require("../models/blacklist.model")
 
 
 async function authMiddleware(req, res, next) {
